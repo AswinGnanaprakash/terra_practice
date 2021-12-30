6a0071b3dd16dd4ed8cedf37dcd45c1d98f52b60
AswinG <aswin@chadura.com>
2021-12-30 18:12:42 +0530

    query and execute mint lines uncommanded

--- terra_excersices/timestamp-handle/src/contract.rs
+++ terra_excersices/timestamp-handle/src/contract.rs
@@ -47,32 +47,32 @@ pub fn instantiate(
 }
 
 
-// #[cfg_attr(not(feature = "library"), entry_point)]
-// pub fn execute(
-//     deps: DepsMut,
-//     _env: Env,
-//     info: MessageInfo,
-//     msg: ExecuteMsg,
-// ) -> Result<Response, ContractError> {
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
 
-//     match msg {
-//         ExecuteMsg::Mint {recipient, amount} => execute_mint_local(deps, _env, info, recipient, amount),
-//     }
-// }
-
-
-// pub fn execute_mint_local(deps: DepsMut,_env:Env, info:MessageInfo, recipient:String, amount:Uint128) -> Result<Response, ContractError>{
-
-//     let timenow = "27 Jun 2021 10AM".to_string();
-//     let rcpt_addr = deps.api.addr_validate(&recipient)?;
-//     TimeStamp.save(deps.storage, &rcpt_addr, &timenow );
-//     execute_mint(deps, _env, info, recipient, amount)
-// }
+    match msg {
+        ExecuteMsg::Mint {recipient, amount} => execute_mint_local(deps, _env, info, recipient, amount),
+    }
+}
 
 
-// #[cfg_attr(not(feature = "library"), entry_point)]
-// pub fn query(deps: Deps, _env: Env, msg: QueryMsg ) -> StdResult<Binary> {
-//     match msg {
-//         QueryMsg::Balance {address} => to_binary(&query_balance(deps, address)?),
-//     }
-// }
+pub fn execute_mint_local(deps: DepsMut,_env:Env, info:MessageInfo, recipient:String, amount:Uint128) -> Result<Response, ContractError>{
+
+    let timenow = "27 Jun 2021 10AM".to_string();
+    let rcpt_addr = deps.api.addr_validate(&recipient)?;
+    TimeStamp.save(deps.storage, &rcpt_addr, &timenow );
+    execute_mint(deps, _env, info, recipient, amount)
+}
+
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg ) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::Balance {address} => to_binary(&query_balance(deps, address)?),
+    }
+}
